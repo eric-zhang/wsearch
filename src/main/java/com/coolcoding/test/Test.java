487c667a4c1ae2dc01115397ace255d93b74886c
eric_zhang <jianfu.zhang@qq.com>
2014-12-16 00:16:31 +0800

    search 初始提交

--- src/main/java/com/coolcoding/test/Test.java
+++ src/main/java/com/coolcoding/test/Test.java
@@ -4,4 +4,22 @@ package com.coolcoding.test;
  * Created by Administrator on 2014/12/15.
  */
 public class Test {
+    private String id;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
